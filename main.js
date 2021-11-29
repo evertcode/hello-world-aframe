0175ed46c835c58901cc83ac23e7b767c3007e46
Hebert Hernandez <hebert.hernandez7@gmail.com>
2021-11-29 12:38:48 -0600

    add sky

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 import 'aframe'
 
 document.querySelector('#app').innerHTML = `
-  <a-scene>
-    <a-box position="1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-    <a-sphere position="0 1.2 -6" radius="1.25" color="#EF2D5E">
-    </a-sphere>
-    <a-cylinder position="-1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-    <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-    <a-sky color="#ECECEC"></a-sky>
+  <a-scene background='color: #FAFAFA'>
+    
+    <a-assets>
+      <img id='sky1' src='/sky.jpg' alt='Sky' />
+    </a-assets>
+
+    <a-entity
+      id='cam'
+      camera='fov:80'
+      position='0 1.6 0'
+      look-controls>
+
+      <a-plane
+        id='camfadeplane'
+        rotation='10 0.5 0'
+        position='0 0 -0.5'
+        material='color:#000000; transparent:true; opacity:0'
+        width='2'
+        height='2'
+        animation__fadein='property:material.opacity; to:1; dur:2000;'
+        animation__fadeout='property:material.opacity; to:0; dur:200;'>
+      </a-plane>
+
+    </a-entity>
+
+    <a-sky id='skybox' src='#sky1'>
+    </a-sky>
+
   </a-scene>
 `
